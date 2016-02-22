ce3b3ad3838403a8f40cb7bcf1d2601ee876291d
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-22 21:57:36 +0900

    Add print-object for dnn

--- src/dnn.lisp
+++ src/dnn.lisp
@@ -26,5 +26,13 @@
                          :accessor dnn-learning-coefficient)
    (mini-batch-size :initform 10)))
 
+(defmethod print-object ((dnn dnn) stream)
+  (print-unreadable-object (dnn stream :type t :identity t)
+    (with-slots (learning-coefficient mini-batch-size) dnn
+      (format stream
+              ":LEARING-COEFFICIENT ~a :MINI-BATCH-SIZE ~a"
+              learning-coefficient
+              mini-batch-size))))
+
 (defun predict (dnn input)
   (declare (ignore dnn input)))
