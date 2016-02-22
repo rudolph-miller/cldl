639303c83ef86479d3f6e0ca2443644a862f1ce2
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-22 14:09:49 +0900

    Add connection class

--- src/connection.lisp
+++ src/connection.lisp
@@ -2,8 +2,39 @@
 (defpackage cldl.connection
   (:use #:cl
         #:annot.doc
-        #:annot.class))
+        #:annot.class)
+  (:import-from #:cldl.unit
+                #:unit))
 (in-package :cldl.connection)
 
 (syntax:use-syntax :annot)
-        
+
+@export
+@export-accessors
+@doc
+"Class of connection"
+(defclass connection ()
+  ((left-unit :initform nil
+              :type (or null unit)
+              :initarg :left-unit
+              :accessor connection-left-unit)
+   (right-unit :initform nil
+               :type (or null unit)
+               :initarg :right-unit
+               :accessor connection-right-unit)
+   (weight :initform 1
+           :type number
+           :initarg :weight
+           :accessor connection-weight)
+   (weight-diff :initform 0
+                :type number
+                :initarg :weight-diff
+                :accessor connection-weight-diff)))
+
+(defmethod print-object ((connection connection) stream)
+  (print-unreadable-object (connection stream :type t :identity t)
+    (with-slots (weight weight-diff) connection
+      (format stream
+              ":WEIGHT ~a :WEIGHT-DIFF ~a"
+              weight
+              weight-diff))))
