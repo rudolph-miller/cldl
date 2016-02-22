b060db660c145e3e97269884e90f7944d74e305e
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-22 13:54:49 +0900

    Add unit class

--- src/unit.lisp
+++ src/unit.lisp
@@ -1,7 +1,53 @@
 (in-package :cl-user)
 (defpackage cldl.unit
   (:use #:cl
-        #:annot.doc))
+        #:annot.doc
+        #:annot.class))
 (in-package :cldl.unit)
 
 (syntax:use-syntax :annot)
+
+@export
+@export-accessors
+@doc
+"Base class for Unit."
+(defclass unit ()
+  ((left-connections :initform nil
+                     :type list
+                     :initarg :left-connections
+                     :accessor unit-left-connections)
+   (right-connections :initform nil
+                      :type list
+                      :initarg :right-connections
+                      :accessor unit-right-connections)
+   (input-value :initform 0
+                :type number
+                :initarg :input-value
+                :accessor unit-input-value)
+   (output-value :initform 0
+                 :type number
+                 :initarg :output-value
+                 :accessor unit-output-value)
+   (delta :initform 0
+          :type number
+          :initarg :delta
+          :accessor unit-delta)))
+
+(defmethod print-object ((unit unit) stream)
+  (print-unreadable-object (unit stream :type t :identity t)
+    (with-slots (input-value output-value delta) unit
+      (format stream
+              ":INPUT ~a :OUTPUT ~a :DELTA ~a"
+              input-value output-value delta))))
+
+@export
+(defclass input-unit (unit) ())
+
+@export
+(defclass hidden-unit (unit) ())
+
+@export
+(defclass bias-unit (unit) ())
+
+@export
+(defclass output-unit (unit) ())
