643ca9f04727a8495cc90b32c75130b658c22697
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-22 17:57:32 +0900

    Add dnn class

--- src/dnn.lisp
+++ src/dnn.lisp
@@ -1,10 +1,32 @@
 (in-package :cl-user)
 (defpackage cldl.dnn
   (:use #:cl
-        #:annot.doc))
+        #:annot.doc
+        #:annot.class))
 (in-package :cldl.dnn)
 
 (syntax:use-syntax :annot)
+
+(defvar +DEFAULT-WEIGHT+ 0)
+
+@export
+@export-accessors
+@doc
+"Class of Deep Neural Network."
+(defclass dnn ()
+  ((units :initform nil
+          :type list
+          :initarg :units
+          :accessor dnn-units)
+   (connections :initform nil
+                :type list
+                :initarg :connections
+                :accessor dnn-connections)
+   (learning-coefficient :initform 0.01
+                         :type number
+                         :initarg :learning-coefficient
+                         :accessor dnn-learning-coefficient)
+   (mini-batch-size :initform 10)))
 
 (defun predict (dnn input)
   (declare (ignore dnn input)))
