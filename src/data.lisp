b376c3b03154047efafdda9a9e5fe4312b2c289d
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-23 00:36:52 +0900

    Add normalize-data-set

--- src/data.lisp
+++ src/data.lisp
@@ -2,7 +2,10 @@
 (defpackage cldl.data
   (:use #:cl
         #:annot.doc
-        #:annot.class))
+        #:annot.class)
+  (:import-from #:cldl.util
+                #:mean-and-standard-deviation
+                #:normalize))
 (in-package :cldl.data)
 
 (syntax:use-syntax :annot)
@@ -35,3 +38,33 @@
   (mapcar #'(lambda (data)
               (apply #'make-data data))
           list))
+
+@export
+(defun normalize-data-set (data-set)
+  (let* ((length (length (data-input (car data-set))))
+         (t-array (make-array length :initial-element nil))
+         means-and-standard-deviations)
+    (loop for data in data-set
+          for i from 0
+          do (loop for value in (data-input data)
+                   for j from 0
+                   do (push value (aref t-array j))))
+    (loop for i from 0 below length
+          for list = (aref t-array i)
+          do (multiple-value-bind (mean standard-deviation)
+                 (mean-and-standard-deviation list)
+               (setq means-and-standard-deviations
+                     (append (list (cons mean standard-deviation))
+                             means-and-standard-deviations))))
+    (mapcar #'(lambda (data)
+                (let ((input
+                        (loop for value in (data-input data)
+                              for mean-and-standard-deviation
+                                in means-and-standard-deviations
+                              for mean = (car mean-and-standard-deviation)
+                              for standard-deviation = (cdr mean-and-standard-deviation)
+                              collecting (normalize value mean standard-deviation))))
+                  (make-instance 'data
+                                 :input input
+                                 :expected (data-expected data))))
+            data-set)))
