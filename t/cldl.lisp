ea5ba38fa53d2f62be9796ac70a5a8f3b60544ee
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-22 23:44:53 +0900

    Add test function

--- t/cldl.lisp
+++ t/cldl.lisp
@@ -1,12 +1,22 @@
 (in-package :cl-user)
 (defpackage cldl-test
   (:use #:cl
-        #:cldl
         #:prove)
   (:import-from #:split-sequence
                 #:split-sequence)
+  (:import-from #:cldl.unit
+                #:generate-units
+                #:output-units)
+  (:import-from #:cldl.connection
+                #:connect)
   (:import-from #:cldl.data
-                #:make-data-set))
+                #:make-data-set
+                #:data-input
+                #:data-expected)
+  (:import-from #:cldl.dnn
+                #:dnn
+                #:dnn-units
+                #:predict))
 (in-package :cldl-test)
 
 (plan nil)
@@ -41,5 +51,21 @@
                (list :expected (car data)
                      :input (cdr data)))
            (data))))
+
+(defun test ()
+  (let* ((units (generate-units (list 4 4 4 3)))
+         (connections (connect units))
+         (data-set (data-set))
+         (dnn (make-instance 'dnn :units units :connections connections))
+         (correc-count 0))
+    (dolist (data data-set)
+      (when (= (position (predict dnn (data-input data))
+                         (output-units (dnn-units dnn)))
+               (data-expected data))
+        (incf correc-count)))
+    (let ((rate (/ correc-count (length data-set))))
+      (format t "Accuracy: ~,2f%" rate)
+      rate)))
+
 
 (finalize)
