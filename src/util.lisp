1c1211ab2511e905df6518184bbc9b134413d11f
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-22 11:05:30 +0900

    Add normal-random

--- src/util.lisp
+++ src/util.lisp
@@ -5,3 +5,21 @@
 (in-package :cldl.util)
 
 (syntax:use-syntax :annot)
+
+(defvar +random-digits+ (expt 10 24))
+
+(setf *random-state* (make-random-state t))
+
+(defun random<1 ()
+  (/ (random +random-digits+) +random-digits+))
+
+@export
+@doc
+"Get a random number from normal distribution."
+(defun normal-random (mean standard-deviation)
+  (let ((x (random<1))
+        (y (random<1)))
+    (+ mean
+       (* standard-deviation
+          (sqrt (* -2 (log x)))
+          (cos (* 2 pi y))))))
